d858ff6680f9f03def4e449c97f00cee534e1133
paulscott56 <pongid56>
2016-03-28 10:49:37 +0200

    magic mirror basic interface release apk

--- app/src/main/java/za/co/paulscott/magicmirror/FullscreenActivity.java
+++ app/src/main/java/za/co/paulscott/magicmirror/FullscreenActivity.java
@@ -1,10 +1,9 @@
 package za.co.paulscott.magicmirror;
 
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
